e3f2d5e84bff1afec17a066cb1f95b6b71d83dc6
Prajakta <prajaktas811@gmail.com>
2024-10-07 14:07:37 +0530

    Added new Prime Array

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 ,const numbers=[19,23,4,16,28,15,31,8,29,14,6,35,2,11,17,5,9,27,12,30]
 let count=0;
+let primArray=[]
 for( let i=0;i<numbers.length;i++)
 { bag="";
   for(let j=1;j<=numbers.length;j++)
@@ -12,4 +13,7 @@ for( let i=0;i<numbers.length;i++)
 if (count==2)
 { bag+=number[i];
     console.log("Prime Numbers "+bag)
+     primArray.push(number[i]);
 }
+
+
